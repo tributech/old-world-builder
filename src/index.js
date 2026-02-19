91193aa6da7f5d3b637bca20e110867b14595c88
Colin Goudie <colin.goudie@tributech.com.au>
2026-02-19 20:04:40 +1100

    Remove PWA install prompt and service worker
    - Remove InstallPWA component from Page footer
    - Remove serviceWorkerRegistration import and register() call

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { HelmetProvider } from "react-helmet-async";
 import * as Sentry from "@sentry/react";
 
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { App } from "./App";
 import store from "./store";
 
@@ -98,4 +97,4 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-serviceWorkerRegistration.register();
+// PWA service worker disabled - OWR uses native mobile apps instead
