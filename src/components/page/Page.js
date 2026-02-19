c115c54882e34eb9f80df6f73841cd8eb3b1db50
Colin Goudie <colin.goudie@tributech.com.au>
2026-02-19 20:04:40 +1100

    Remove PWA install prompt, service worker, and related dead code
    - Remove InstallPWA component from Page footer
    - Remove serviceWorkerRegistration import and register() call
    - Delete service-worker.js and serviceWorkerRegistration.js
    - Delete install-pwa component directory
    - Remove InstallPwa/beforeinstallprompt comment from Header
    - Remove pwa.button and pwa.prompt i18n keys from all 7 languages

--- src/components/page/Page.js
+++ src/components/page/Page.js
@@ -14,7 +14,6 @@ import italy from "../../assets/italy.svg";
 import polen from "../../assets/polen.svg";
 import china from "../../assets/china.svg";
 
-import { InstallPWA } from "../install-pwa";
 import "./Page.css";
 
 export const Main = ({ className, children, isDesktop, compact, loading }) => {
@@ -46,7 +45,6 @@ export const Main = ({ className, children, isDesktop, compact, loading }) => {
             <Link to="/help">
               <FormattedMessage id="footer.help" />
             </Link>
-            <InstallPWA />
             <Link to="/changelog">
               <FormattedMessage id="footer.changelog" />
             </Link>
